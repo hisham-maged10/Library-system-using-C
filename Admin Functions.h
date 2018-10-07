69f58052da746c32e81ee1d50c7ba09cde13d73c
Hisham Maged <hisham.maged10@gmail.com>
2018-10-07 13:46:38 +0200

    Update Admin Functions.h

--- Admin Functions.h
+++ Admin Functions.h
@@ -1,6 +1,10 @@
 #ifndef ADMIN_FUNCTIONS_H_INCLUDED
 #define ADMIN_FUNCTIONS_H_INCLUDED
-
+/**
+ * @author ${hisham_maged10}
+ *https://github.com/hisham-maged10
+ * ${DesktopApps}
+ */
 void overdue();
 void popular();
 void swapnumbers();
